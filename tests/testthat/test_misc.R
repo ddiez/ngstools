06f7eb4c99bf29195edfe3b7d66ba1a60c685c41
Diego Diez <diego10ruiz@gmail.com>
2016-11-12 23:18:34 +0900

    Unit test code reorganization. Use expect_is() to check for class.

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -1,11 +1,11 @@
+context("Fragment size")
+
 f <- system.file("files/example.bam", package = "ngstools")
 tmp <- readGAlignmentPairs(f)
 fs <- fsize(tmp)
 
-context("Fragment size")
-test_that("Compute fragment size", {
-  expect_true(class(tmp) == "GAlignmentPairs")
-  expect_type(fs, "integer")
+test_that("fragment size is correct class and value", {
+  expect_is(fs, "integer")
   expect_true(is.na(fs[1]))
   expect_identical(fs[2], 104L)
 })
